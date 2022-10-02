defb2b8943f77d1b666ad8676ac7aebbe103709c
VitalijO <onyshko.vitalij@gmail.com>
2022-10-02 16:45:37 +0300

    commit
    add gallery task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,28 @@
 // Add imports above this line
+import { galleryItems } from './gallery-items.js';
 import simpleLightbox from 'simplelightbox';
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import "simplelightbox/dist/simple-lightbox.min.css"
 
-console.log(galleryItems);
-console.log(simpleLightbox);
+
+const galleryEls = document.querySelector('.gallery');
+const markupGaleryEls = createMarkupGalleryEls(galleryItems) 
+galleryEls.insertAdjacentHTML('beforeend', markupGaleryEls)
+
+function createMarkupGalleryEls(elements) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <a class="gallery__item"
+        href= "${original}">
+  <img class="gallery__image"
+  src="${preview}" 
+  alt="${description}" />
+</a>`
+    })
+    .join('');
+}
+
+
+const lightbox = new SimpleLightbox('.gallery__item', {
+    fadeSpeed: 250,
+    captionsData:"alt",
+});
